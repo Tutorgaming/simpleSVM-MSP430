aa53b7250b6fe9ffe5114916c80da2b7efd0ef1f
[TUC]Theppasith N <tutorgaming@gmail.com>
2015-06-19 14:28:29 +0200

    Classification works with fixed dimensional array

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 char inputArray[11];
+double sum_temp = 0;
 volatile int ptr;
 volatile int i = 0 ;
 volatile int input_enable=1;
@@ -80,6 +81,28 @@ void println(char *input , int input_size){
 	uart_putchar('\r');
 }
 
+double dot(const svm_node *px, const svm_node *py){
+		sum_temp = 0;
+
+		while(px->index != -1 && py->index != -1)
+		{
+			if(px->index == py->index)
+			{
+				sum_temp += px->value * py->value;
+				++px;
+				++py;
+			}
+			else
+			{
+				if(px->index > py->index)
+					++py;
+				else
+					++px;
+			}
+		}
+		return sum_temp;
+}
+
 void main(void){
 		//Stop Watchdog timer
 			WDTCTL = WDTPW + WDTHOLD;
@@ -131,13 +154,10 @@ void main(void){
 
 		//For Model
 		//double **sv_coef = (double**) malloc((total_sv)*sizeof(double*));
-		double sv_coef[2][9];
-		svm_node SV[9][3];
 
-		//For Classification
-		double *kvalue = (double *)malloc ((total_sv)*sizeof(double));
-		int *start = (int *)malloc ((nr_class)*sizeof(int));
-		int *vote = (int *)malloc ((nr_class)*sizeof(int));
+		double sv_coef[2][9];
+		svm_node SV[9][4];
+
 		int NUM_OF_SV_COEF_ELEMENT = nr_class -1 ;
 
 		it =0 ;
@@ -161,7 +181,7 @@ void main(void){
 				sendACK();
 			}
 			int lastValue;
-			for(it = 0;it<size_of_sv_i-1;it++){
+			for(it = 0;it<size_of_sv_i;it++){
 				while(input_enable ==1);
 				SV[it_i][it].index = (int)atoi(inputArray);
 				sendACK();
@@ -172,6 +192,87 @@ void main(void){
 			}
 		}
 		P4OUT = 0b10000000;
+
+
+
+		//For Classification
+
+		double *dec_values = (double *) malloc((rhosize)*sizeof(double));
+		svm_node x[3];
+		//RESULT==2 1:0.680000 2:0.884000 3:0.000800
+		x[0].index = 1;
+		x[0].value = 0.680000;
+		x[1].index = 2;
+		x[1].value = 0.884000;
+		x[2].index = 3;
+		x[2].value = 0.000800;
+
+
+		//RESULT == 1 1:0.768000 2:0.690000 3:0.034800
+//		x[0].index = 1;
+//		x[0].value = 0.768000;
+//		x[1].index = 2;
+//		x[1].value = 0.694000;
+//		x[2].index = 3;
+//		x[2].value = 0.000800;
+
+		sum_temp = 0;
+		//double *kvalue = Malloc(double,l);
+		double *kvalue = (double *)malloc ((total_sv)*sizeof(double));
+		for(it=0;it<total_sv;it++)
+			kvalue[it] = dot(x,SV[it]); //Kernel::k_function(x,model->SV[i],model->param);
+		sum_temp = 0;
+		//int *start = Malloc(int,nr_class);
+		int *start = (int *)malloc ((nr_class)*sizeof(int));
+		start[0] = 0;
+		for(it=1;it<nr_class;it++)
+			start[it] = start[it-1]+nr_sv[it-1];
+
+		//int *vote = Malloc(int,nr_class);
+		int *vote = (int *)malloc ((nr_class)*sizeof(int));
+		for(it=0;it<nr_class;it++)
+			vote[it] = 0;
+
+		int p=0;
+		double *coef1,*coef2;
+		for(it=0;it<nr_class;it++)
+			for(it_j = it+1;it_j<nr_class;it_j++)
+			{
+				double sum = 0;
+				int si = start[it];
+				int sj = start[it_j];
+				int ci = nr_sv[it];
+				int cj = nr_sv[it_j];
+
+				int k;
+				coef1 = &sv_coef[it_j-1];
+				coef2 = &sv_coef[it];
+				for(k=0;k<ci;k++)
+					sum += coef1[si+k] * kvalue[si+k];
+				for(k=0;k<cj;k++)
+					sum += coef2[sj+k] * kvalue[sj+k];
+				sum -= rho[p];
+				dec_values[p] = sum;
+
+				if(dec_values[p] > 0)
+					++vote[it];
+				else
+					++vote[it_j];
+				p++;
+			}
+
+		int vote_max_idx = 0;
+		for(i=1;i<nr_class;i++)
+			if(vote[i] > vote[vote_max_idx])
+				vote_max_idx = i;
+
+		//free(kvalue);
+		//free(start);
+		//free(vote);
+		int result = label[vote_max_idx];
+		free(dec_values);
+		if(result ==99)P1OUT = 0b00000001;
+
 
 }
 
