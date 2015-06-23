04e6e1b69744f39077ab82efe3b8d29370173942
[TUC]Theppasith N <tutorgaming@gmail.com>
2015-06-23 14:48:35 +0200

    Working Solution with input testing receiver

--- main.c
+++ main.c
@@ -1,29 +1,38 @@
 /*
- * UART MSP430F5529 SERIAL STORE
- * by C3MX.
- * BAUDRATE : 9600
- * SMCLK : 1.5Mhz
+ * LIBSVM LINEAR KERNEL - "Model" C_SVM Implementation
+ * on MSP430F5529
+ * Version 1.0
+ * by Theppasith N.
+ * ( TU-Chemnitz Summer Internship Program )
+ * Serial Comm Detail
+ * 	BAUDRATE : 9600
+ * 	SMCLK    : 1.5Mhz
+ * 	Credited to : LIBSVM library.
  */
 
 #include <msp430f5529.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char inputArray[11];
-double sum_temp = 0;
-volatile int ptr;
-volatile int i = 0 ,result = 9;
-volatile int input_enable=1;
-volatile double multiply_temp = 0;
 
-unsigned int selector = 0;
+char inputArray[11]; // Buffer For Numbers
+double sum_temp = 0; // used in DOT Function
+double sum = 0;		// Voting Process
+unsigned int expect =0; // Expecting Result
+volatile int result = -1; // Result
 
-//data structure for model
+volatile int ptr; //All Purpose Pointer
+volatile int i = 0 ; //All Purpose Pointer
+volatile int input_enable=1; //Input Enable
+volatile double multiply_temp = 0; // used in Multiply Function
+
+//DATA STRUCTURE FOR MODEL
 typedef struct _svm_node_
 {
 	int index;
 	double value;
 } svm_node;
+
 
 void initialize(){
 	 // P3.3,4 = USCI_A1 TXD/RXD (USB TX RX on PORT 3.3,3.4)
@@ -45,6 +54,7 @@ void initialize(){
 	 P4OUT = 0b00000000;
 }
 
+//UART SERIAL COMMUNICATION //////////////////
 void uart_putchar(char input){
 	while (!(UCA1IFG&UCTXIFG));
 	UCA1TXBUF = input;
@@ -53,38 +63,39 @@ void uart_newline(){
 	uart_putchar('\n');
 	uart_putchar('\r');
 }
-
-void sendACK(){
-	uart_putchar('@');
-	uart_newline();
-	ptr=0;
-	input_enable = 1;
-	inputArray[0] = ' ';
-	inputArray[1] = ' ';
-	inputArray[2] = ' ';
-	inputArray[3] = ' ';
-	inputArray[4] = ' ';
-	inputArray[5] = ' ';
-	inputArray[6] = ' ';
-	inputArray[7] = ' ';
-	inputArray[8] = ' ';
-	inputArray[9] = ' ';
-	inputArray[10] = ' ';
-	P1OUT = 0b00000000;
-	P4OUT = 0b00000000;
-}
-
 void println(char *input , int input_size){
 	int i = input_size;
 	for(; i>0 ; i--)uart_putchar(input[input_size-i]);
 	uart_putchar('\n');
 	uart_putchar('\r');
 }
+/////////////////////////////////////////////
+
+void sendACK(){
+	uart_putchar('@');
+	uart_newline();
+	ptr=0;
+	input_enable = 1;
+	//CLEAR BUFFER FOR NEXT INPUT
+		inputArray[0] = ' ';
+		inputArray[1] = ' ';
+		inputArray[2] = ' ';
+		inputArray[3] = ' ';
+		inputArray[4] = ' ';
+		inputArray[5] = ' ';
+		inputArray[6] = ' ';
+		inputArray[7] = ' ';
+		inputArray[8] = ' ';
+		inputArray[9] = ' ';
+		inputArray[10] = ' ';
+	//TURN LED OFF
+		P1OUT = 0b00000000;
+}
 
 double multiply(double *left , double *right){
-	multiply_temp =0;
-	multiply_temp = (float)(*left) * (float)(*right);
-	return multiply_temp;
+		multiply_temp =0;
+		multiply_temp = (float)(*left) * (float)(*right);
+		return multiply_temp;
 }
 
 double dot(const svm_node *px, const svm_node *py){
@@ -106,6 +117,9 @@ double dot(const svm_node *px, const svm_node *py){
 }
 
 void main(void){
+	//////////////////////////////////////////
+	//////       INITIALIZATION          /////
+	//////////////////////////////////////////
 		//Stop Watchdog timer
 			WDTCTL = WDTPW + WDTHOLD;
 		//Initialize Register for UART and LED
@@ -113,78 +127,62 @@ void main(void){
  	 	//TRANSMISSION TEST
 			char * message = "HELLO WORLD :D ";
 			println(message,strlen(message));
-
 		//Receiver Interrupt
 			__bis_SR_register(GIE); //interrupts enabled
 			__no_operation();
 
-		//RECEIVE PARAMETERS
+	//////////////////////////////////////////
+	//////       RECEIVE PARAMETERS      /////
+	//////////////////////////////////////////
 
-		//NUMBER OF DIMENSION
+		// Number of INPUT_DIMENSION
 			while(input_enable == 1);
 				int dimension = atoi(inputArray);
 			sendACK(); //Send @ as ACK signal to get the next Value
-
-		//NUMBER OF CLASSES
+		// Number of CLASSES
 			while(input_enable == 1);
 				int nr_class = atoi(inputArray);
-			sendACK(); //Send @ as ACK signal to get the next Value
-
-		//NUMBER OF TOTAL SUPPORT VECTORS
+			sendACK();
+		// Number of TOTAL_Support Vectors
 			while(input_enable == 1);
 				int total_sv = atoi(inputArray);
 			sendACK();
-
-		//RHO Array
+		// RHO Array (for Voting process)
 			int rhosize = nr_class*(nr_class-1)/2;
 			double *rho = (double *)malloc((rhosize)*sizeof(double));
-			unsigned int it=0;
+		unsigned int it=0; //ALL PURPOSE POINTER
 				for(; it < rhosize ; it++){
 					while(input_enable == 1);
 						rho[it] = atof(inputArray);
 					sendACK();
 				}
-
-		//CLASS LABEL TAG
-		it=0;
+		// Class Name Tag Array
 			int *label = (int *)malloc((nr_class)*sizeof(int));
-			for(; it < nr_class ; it++){
+			for(it=0; it < nr_class ; it++){
 				while(input_enable == 1);
 					label[it] = atoi(inputArray);
 				sendACK();
 			}
-
-		//NR_SV
-		it=0;
-		//model->nSV = Malloc(int,nr_class);
+		// model->nSV (for Voting Process 'start[]')
 			int *nr_sv = (int*)malloc((nr_class)*sizeof(int));
-			for(; it < nr_class ; it++){
+			for(it=0; it < nr_class ; it++){
 				while(input_enable == 1);
 				nr_sv[it] = atoi(inputArray);
 				sendACK();
 			}
 
-		int NUM_OF_SV_COEF_ELEMENT = nr_class -1 ;
-
 		//Support Vector and its Coefficient
-
-		//double sv_coef[2][9];
-		//svm_node SV[9][4];
 
 		svm_node **SV = (svm_node **) malloc ((total_sv)*sizeof(svm_node*));
 		int z = 0;
 		for(it=0 ; it < total_sv ; it++){
 			SV[it] = (svm_node*) malloc ((dimension+1)*sizeof(svm_node));
-			//SV[it] = (svm_node*) malloc ((4)*sizeof(svm_node));
 			for(z=0;z < dimension+1 ; z++){
-				//for(z=0;z < 4; z++){
 				SV[it][z].index = 0;
 				SV[it][z].value = 0.0;
 			}
 		}
 
-		z=0;
-		it=0;
 		double **sv_coef = (double**) malloc((nr_class-1)*sizeof(double*)); //double **sv_coef = (double**) malloc((2)*sizeof(double*));
 		for(it=0; it < (nr_class-1);it++){
 			sv_coef[it] = (double*) malloc (total_sv * sizeof(double));
@@ -194,6 +192,7 @@ void main(void){
 		}
 
 		it =0 ;
+		int NUM_OF_SV_COEF_ELEMENT = nr_class -1 ;
 		int it_i=0;
 		int it_j=0;
 		for(it_i=0;it_i<total_sv ;it_i++){
@@ -203,14 +202,14 @@ void main(void){
 			int size_of_sv_i = atoi(inputArray);
 			sendACK();
 
-			//SV_COEF
+			// Inputs for SV_COEFFICIENT
 			for(it_j=0;it_j<NUM_OF_SV_COEF_ELEMENT;it_j++){
 				while(input_enable ==1);
 				sv_coef[it_j][it_i] = atof(inputArray);
 				sendACK();
 			}
 
-			//SV
+			// Inputs for Support Vectors (SV[][]) in 'svm_node' format
 			for(it = 0;it<size_of_sv_i;it++){
 				//index
 				while(input_enable ==1);
@@ -222,84 +221,78 @@ void main(void){
 				sendACK();
 			}
 		}
-		//GREEN LED SHOW THE INPUT PROCESS HAS BEEN ENDED.
-		P4OUT = 0b10000000;
 
-		//LINEAR Classification Process
+		//GREEN LED SHOW THAT MODEL HAS FINISHED LOADING
+				P4OUT = 0b10000000;
+				P1OUT = 0b00000000;
+
+		//////////////////////////////////
+		//       TEST VALUE INPUT
+		//////////////////////////////////
+		svm_node *x = (svm_node *) malloc((dimension+1)*sizeof(svm_node));
+
+		for(it = 0;it<dimension;it++){
+			x[it].index = it+1;
+			while(input_enable ==1);
+				x[it].value = (double)atof(inputArray);
+			sendACK();
+		}
+		x[it].index = -1;
+		x[it].value = 0.000000;
+
+		//Expect Result Value
+		while(input_enable ==1);
+			expect = (int)atoi(inputArray);
+		sendACK();
+
+		//////////////////////////////////
+		// LINEAR Classification Process
+		//////////////////////////////////
+
+		// Decision Value (For voting process)
 		double *dec_values = (double *) malloc((rhosize)*sizeof(double));
 
-
-		P1OUT = 0b00000000;
-
-		//TEST VALUE INPUT
-		svm_node x[3];
-		//RESULT==2 1:0.680000 2:0.884000 3:0.000800
-		if(selector == 2){
-		x[0].index = 1;
-		x[0].value = 0.680000;
-		x[1].index = 2;
-		x[1].value = 0.884000;
-		x[2].index = 3;
-		x[2].value = 0.000800;
-		}
-//		//RESULT == 1 1:0.768000 2:0.690000 3:0.034800
-		if(selector == 1){
-		x[0].index = 1;
-		x[0].value = 0.768000;
-		x[1].index = 2;
-		x[1].value = 0.694000;
-		x[2].index = 3;
-		x[2].value = 0.000800;
-		}
-		//0 1:0.528000 2:0.668000 3:0.834800
-		if(selector == 0){
-		x[0].index = 1;
-		x[0].value = 0.528000;
-		x[1].index = 2;
-		x[1].value = 0.668000;
-		x[2].index = 3;
-		x[2].value = 0.834800;
-		}
-
-
+		// K-Value ( Calculation between input vector X and SUPPORT VECTOR )
+		// There are a lot of equations
+		// this is the DOT method. (C_SVM - Linear)
 		double *kvalue = (double *)malloc ((total_sv)*sizeof(double));
 		for(it=0;it<total_sv;it++)
 			kvalue[it] = dot(x,SV[it]);
 
-
+		// Array to store the pre-calculated position of pointer to be.
 		int *start = (int *)malloc ((nr_class)*sizeof(int));
 		start[0] = 0;
 		for(it=1;it<nr_class;it++)
 			start[it] = start[it-1]+nr_sv[it-1];
 
-		//VOTING ARRAY (decided which class that the 'X' data should be)
+		// VOTING ARRAY (decided which class that the 'X' data should be)
 		int *vote = (int *)malloc ((nr_class)*sizeof(int));
 		for(it=0;it<nr_class;it++)
 			vote[it] = 0;
 
-		//VOTING PROCESS
+		// VOTING PROCESS
 		int p=0;
-		double *coef1,*coef2,sum;
+		double *coef1,*coef2;
 		int si,sj,ci,cj,k,vote_max_idx;
-
 		for(it=0;it<nr_class;it++){
 			for(it_j = it+1;it_j<nr_class;it_j++){
 				sum = 0;
+				//Iterator Things
 				si = start[it];
 				sj = start[it_j];
 				ci = nr_sv[it];
 				cj = nr_sv[it_j];
 
-				coef1 = sv_coef[it_j-1]; //coef1 = &sv_coef[it_j-1]; //ARRAY IMPLEMENTATION
-				coef2 = sv_coef[it]; //coef2 = &sv_coef[it];    //ARRAY IMPLEMENTATION
+				coef1 = sv_coef[it_j-1];
+				coef2 = sv_coef[it];
 
 				for(k=0;k<ci;k++)
 					sum+= multiply(&coef1[si+k],&kvalue[si+k]); //	sum += coef1[si+k] * kvalue[si+k];
 				for(k=0;k<cj;k++)
-					sum += coef2[sj+k] * kvalue[sj+k];
+					sum+= multiply(&coef2[sj+k],&kvalue[sj+k]); //sum += coef2[sj+k] * kvalue[sj+k];
 				sum -= rho[p];
 				dec_values[p] = sum;
-
+				//Voting From Decision Value
 				if(dec_values[p] > 0)
 					++vote[it];
 				else
@@ -307,11 +300,13 @@ void main(void){
 				p++;
 			}
 		}
+
 		//Find Max Voting Value
 		vote_max_idx = 0;
 		for(i=1;i<nr_class;i++)
 			if(vote[i] > vote[vote_max_idx])vote_max_idx = i;
 
+		//Free The Memories
 		free(kvalue);
 		free(start);
 		free(vote);
@@ -320,12 +315,8 @@ void main(void){
 		result = label[vote_max_idx];
 
 		//TEST EXPECTING RESULT
-		if(result ==selector /*&& SV[0][0].index ==1*/)P1OUT = 0b00000001; //SHOW RED LED when result is correct :D
+		if(result == expect)P1OUT = 0b00000001; //SHOW RED LED when result is correct :D
 		else P1OUT = 0b00000000;
-
-		P2REN = 0x02;         // Turn on pull up resistor to push button
-		P2IE |= BIT1;         // P1.3 interrupt enabled
-		while(1);
 
 }
 
@@ -339,17 +330,9 @@ __interrupt void USCI_A1_ISR(void){
 			input_enable = 0;
 			P1OUT = 0b00000001;
 		}else{
-			//while (!(UCA1IFG&UCTXIFG));
-			//UCA1TXBUF = UCA1RXBUF;
 			inputArray[ptr++] = UCA1RXBUF;
 		}
 	}
 }
 
-#pragma vector=PORT2_VECTOR
-__interrupt void PORT2_ISR(void){
-	sleep(50);
-	selector++;
-	P2IFG &= ~BIT1; // P1.3 IFG cleared
-}
 
